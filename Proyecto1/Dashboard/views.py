914260c0ee96b86f8b3fa9d107a762976d9a101b
gabriela denisse hernandez penagos <183422@ids.upchiapas.edu.mx>
2020-03-26 22:37:19 -0600

    Clase1

---
+++ Proyecto1/Dashboard/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class DashboardClass(LoginRequiredMixin,View):
+    template_ok= 'Dashboard/dashboard.html'
+    def get(self,request, * args, ** kwargs):
+        return render(request,self.template_ok,{})
+
